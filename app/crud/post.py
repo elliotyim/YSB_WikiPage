82fa1a52d133e4cff980d77744a5e83db470ec05
Elliot Yim <yimsb02@naver.com>
2023-11-30 14:54:47 +0900

    Implement retreving post detail

--- app/crud/post.py
+++ app/crud/post.py
@@ -16,48 +16,45 @@ class PostCrud:
         self.db = db
         self.word_processor = word_processor
 
-    def generate_words(self, words: list[str]) -> list[Word]:
-        word_set = set(words)
-
-        existing_words = self.db.query(Word).filter(
-            or_(
-                (Word.name == word for word in words)
-            )
-        ).all()
-
-        for word in existing_words:
-            word_set.remove(word.name)
-
-        return existing_words + [Word(name=word) for word in word_set]
-
     def find_posts(self, page: int, per_page: int) -> list[Post]:
         posts = self.db.query(Post).limit(per_page).offset((page - 1) * per_page).all()
         return posts
-
-    def save_post_and_words(self, post: Post, words: list[Word]) -> list[PostWord]:
-        post_words = [PostWord(post=post, word=word) for word in words]
-        self.db.add_all(post_words)
-        return post_words
 
     def find_post_with_post_words_by_post_id(self, post_id: int) -> Post:
         post = self.db.query(Post).options(joinedload(Post.post_words)).filter(Post.id == post_id).one()
         return post
 
     def find_related_posts(self, base_post_id: int, word_ids: list[int], n: int) -> list[response.Post]:
+        # Word relation score
+        subquery = (
+            select(
+                PostWord.post_id.label("post_id0"),
+                PostWord.word_id.label("word_id0"),
+                func.sum(PostWord.count).label("score")
+            )
+            .select_from(PostWord)
+            .group_by(PostWord.post_id, PostWord.word_id)
+            .subquery()
+        )
+
         related_posts = (
             self.db.query(
-                PostWord.post_id.label("post_id"),
-                Post.title.label("title"),
-                Post.created_at.label("created_date"),
-                func.count(PostWord.post_id)
+                PostWord.post_id,
+                Post.title,
+                Post.created_at,
+                func.sum(subquery.c.score)
             )
             .select_from(PostWord)
             .join(Post)
+            .join(
+                subquery,
+                (PostWord.post_id == subquery.c.post_id0) & (PostWord.word_id == subquery.c.word_id0)
+            )
             .filter(PostWord.post_id != base_post_id)
             .filter(or_((PostWord.word_id == word_id for word_id in word_ids)))
             .group_by(PostWord.post_id, Post.title, Post.created_at)
             .having(func.count(PostWord.post_id) >= 2)
-            .order_by(desc(func.count(PostWord.post_id)), PostWord.post_id)
+            .order_by(desc(func.sum(subquery.c.score)), PostWord.post_id)
             .limit(n)
             .all()
         )
@@ -69,3 +66,14 @@ class PostCrud:
                 created_date=created_date
             ) for post_id, post_title, created_date, count in related_posts
         ]
+
+    def save_post_words(self, post: Post, word_count: dict[Word, int]) -> list[PostWord]:
+        post_words = [
+            PostWord(
+                count=count,
+                post=post,
+                word=word
+            ) for word, count in word_count.items()
+        ]
+        self.db.add_all(post_words)
+        return post_words
--- app/services/post_service.py
+++ app/services/post_service.py
@@ -2,8 +2,10 @@ from fastapi import Depends
 
 from app.configs import get_settings, Settings
 from app.crud.post import PostCrud
+from app.crud.word import WordCrud
 from app.db import Session, get_db
 from app.db.models.post import Post
+from app.db.models.word import Word
 from app.dto.post import request, response
 from app.utils.word_processor import WordProcessor
 
@@ -13,11 +15,13 @@ class PostService:
             self,
             db: Session = Depends(get_db),
             post_crud: PostCrud = Depends(),
+            word_crud: WordCrud = Depends(),
             word_processor: WordProcessor = Depends(),
             settings: Settings = Depends(get_settings)
     ):
         self.db = db
         self.post_crud = post_crud
+        self.word_crud = word_crud
         self.word_processor = word_processor
         self._settings = settings
 
@@ -51,22 +55,27 @@ class PostService:
         )
 
     def create_post(self, request_body: request.PostCreate) -> response.PostCreate:
-        filtered_words = self.word_processor.count_related_words(
+        post = Post(title=request_body.title, content=request_body.content)
+
+        filtered_word_count = self.word_processor.count_related_words(
             content=request_body.content,
             redundant_rate=self._settings.REDUNDANT_RATE
         )
+        filtered_words = list(filtered_word_count.keys())
 
-        words = self.post_crud.generate_words(words=list(filtered_words.keys()))
-        post = Post(title=request_body.title, content=request_body.content)
-        post_words = self.post_crud.save_post_and_words(post=post, words=words)
+        existing_words = self.word_crud.find_words_by_word_name(word_names=filtered_words)
+        new_words = set(filtered_words) - set([word.name for word in existing_words])
 
+        word_count = {word: filtered_word_count[word.name] for word in existing_words}
+        for new_word in new_words:
+            word_count[Word(name=new_word)] = filtered_word_count[new_word]
+
+        post_words = self.post_crud.save_post_words(post=post, word_count=word_count)
         self.db.commit()
 
-        post_dto = response.PostCreate(
+        return response.PostCreate(
             post_id=post.id,
             post_title=post.title,
             post_content=post.content,
             words=[response.Word(word_id=pw.word_id, word=pw.word.name) for pw in post_words]
         )
-
-        return post_dto
